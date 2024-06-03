c562d68cc21561312990beca272b2f5c29258ab7
nguyentrunghieu297 <trunghieunguyen2729@gmail.com>
2024-06-03 21:06:32 +0700

    minor fix

--- src/lib/zod/schema.ts
+++ src/lib/zod/schema.ts
@@ -6,21 +6,21 @@ import { z } from 'zod'
 export const editProfileTemplateSchema = z.object({
   fullName: z
     .string()
-    .min(5, { message: ERROR_MESSAGES.EM45 })
-    .max(40, { message: ERROR_MESSAGES.EM45 })
-    .refine((value) => isNaN(Number(value)), { message: ERROR_MESSAGES.EM3 }),
-  email: z.string({ required_error: ERROR_MESSAGES.EM19 }).email(),
+    .min(5, { message: ERROR_MESSAGES.EM1 })
+    .max(40, { message: ERROR_MESSAGES.EM1 })
+    .refine((value) => isNaN(Number(value)), { message: ERROR_MESSAGES.EM1 }),
+  email: z.string({ required_error: ERROR_MESSAGES.EM1 }).email(),
   address: z
-    .string({ required_error: ERROR_MESSAGES.EM22 })
-    .min(3, { message: ERROR_MESSAGES.EM15 })
-    .max(80, { message: ERROR_MESSAGES.EM15 }),
-  gender: z.enum(['Male', 'Female'], { required_error: ERROR_MESSAGES.EM20 }),
+    .string({ required_error: ERROR_MESSAGES.EM1 })
+    .min(3, { message: ERROR_MESSAGES.EM1 })
+    .max(80, { message: ERROR_MESSAGES.EM1 }),
+  gender: z.enum(['Male', 'Female'], { required_error: ERROR_MESSAGES.EM1 }),
   dob: z
-    .string({ required_error: ERROR_MESSAGES.EM16 })
+    .string({ required_error: ERROR_MESSAGES.EM1 })
     .refine((dob) => differenceInYears(new Date(), new Date(dob)) >= 18, {
       message: 'User must be at least 18 years old',
     }),
-  phone: z.string({ required_error: ERROR_MESSAGES.EM21 }).regex(phoneRegex, { message: ERROR_MESSAGES.EM27 }),
+  phone: z.string({ required_error: ERROR_MESSAGES.EM1 }).regex(phoneRegex, { message: ERROR_MESSAGES.EM1 }),
   role: z.string().readonly(),
   username: z.string().readonly(),
 })
